50dce13bd2f6e2c354d44ec0a5b45aa781d979ff
glooms00 <doha@CTD.ma>
2024-10-28 11:05:32 +0100

    internatonalisation des messages

--- src/main/java/com/example/gestionemployee/GestionEmployeeApplication.java
+++ src/main/java/com/example/gestionemployee/GestionEmployeeApplication.java
@@ -2,6 +2,9 @@ package com.example.gestionemployee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 
 @SpringBootApplication
 public class GestionEmployeeApplication {
@@ -9,5 +12,14 @@ public class GestionEmployeeApplication {
     public static void main(String[] args) {
         SpringApplication.run(GestionEmployeeApplication.class, args);
     }
-
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
 }
+
+
+
