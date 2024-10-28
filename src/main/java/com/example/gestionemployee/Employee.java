d848577c569dc6d3f93458bab2d42212c1b2bd26
glooms00 <doha@CTD.ma>
2024-10-28 10:05:17 +0100

    internatonalisation des messages

--- src/main/java/com/example/gestionemployee/Employee.java
+++ src/main/java/com/example/gestionemployee/Employee.java
@@ -17,6 +17,47 @@ public class Employee {
 
     @Column(unique = true)
     private String email;
-    // Getters, setters, and other methods
-}
 
+    // Default constructor
+    public Employee() {}
+
+    // Constructor with parameters (optional)
+    public Employee(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
